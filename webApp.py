de36ada2d51f4749b44177a4fbc800a901a70378
Tu Nombre <you@example.com>
2023-05-25 23:13:40 -0600

    saltos de linea ejemplo

--- webApp.py
+++ webApp.py
@@ -6,15 +6,12 @@ from glitch_this import ImageGlitcher
 import random
 import io
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     ip_address = socket.gethostbyname(socket.gethostname())
     return render_template('index.html', ip_address=ip_address)
-
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -27,7 +24,6 @@ def upload():
         glitched_image.save('uploads/' + glitched_filename)
         return render_template('index.html', filename=glitched_filename)
     return 'No se seleccionó ninguna imagen'
-
 
 def apply_glitch(path):
     try:
@@ -46,7 +42,6 @@ def apply_glitch(path):
 
     return img  # Retornar la imagen glitcheada en lugar del nombre de archivo
 
-
 def noise(img):
     for i in range(round(img.size[0] * img.size[1] / random.randint(1, 10))):
         img.putpixel(
@@ -62,7 +57,6 @@ def noise(img):
         )
     return img
 
-
 def glitch(img):
     booleanRand = random.randint(0, 1)
     colorOffsetVal = True
@@ -75,11 +69,9 @@ def glitch(img):
     )
     return glitchImage
 
-
 def generate_filename():
     # Generar un nombre único para la imagen
     return f"glitch_{random.randint(1000, 9999)}.jpg"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
