c3d4a5a226056f8e4cedf1446dd26b7c6d4553f7
Tu Nombre <you@example.com>
2023-05-25 23:28:13 -0600

    Codigo comentado-

--- webApp.py
+++ webApp.py
@@ -10,39 +10,54 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Obtenemos la dirección IP del servidor
     ip_address = socket.gethostbyname(socket.gethostname())
+    # Renderizamos la plantilla 'index.html' y pasamos la dirección IP como variable
     return render_template('index.html', ip_address=ip_address)
 
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'image' in request.files:
+        # Obtenemos el archivo de imagen del formulario
         image = request.files['image']
         filename = image.filename
+        # Guardamos el archivo en la carpeta 'uploads' con el nombre original
         image.save('uploads/' + filename)
+        # Aplicamos el efecto de glitch a la imagen
         glitched_image = apply_glitch('uploads/' + filename)
+        # Generamos un nombre para la imagen glitcheada
         glitched_filename = 'glitched_' + filename
+        # Guardamos la imagen glitcheada en la carpeta 'uploads' con el nuevo nombre
         glitched_image.save('uploads/' + glitched_filename)
+        # Renderizamos la plantilla 'index.html' y pasamos el nombre de la imagen glitcheada como variable
         return render_template('index.html', filename=glitched_filename)
+    # Si no se seleccionó ninguna imagen, mostramos un mensaje de error
     return 'No se seleccionó ninguna imagen'
 
 def apply_glitch(path):
     try:
+        # Abrimos la imagen utilizando la biblioteca PIL
         image = PIL.Image.open(path)
     except:
         print("Ruta no válida")
         return
 
+    # Aplicamos el efecto de ruido a la imagen
     img = noise(image)
+    # Aplicamos el efecto de glitch a la imagen
     img = glitch(img)
 
-    # Guardar la imagen con un nombre único
+    # Generamos un nombre único para la imagen glitcheada
     glitched_filename = generate_filename()
+    # Guardamos la imagen glitcheada en la carpeta 'uploads' con el nuevo nombre
     glitched_save_path = os.path.join('uploads', glitched_filename)
     img.save(glitched_save_path)
 
-    return img  # Retornar la imagen glitcheada en lugar del nombre de archivo
+    # Retornamos la imagen glitcheada en lugar del nombre de archivo
+    return img
 
 def noise(img):
+    # Aplicamos el efecto de ruido a la imagen
     for i in range(round(img.size[0] * img.size[1] / random.randint(1, 10))):
         img.putpixel(
             (
@@ -58,6 +73,7 @@ def noise(img):
     return img
 
 def glitch(img):
+    # Aplicamos el efecto de glitch a la imagen
     booleanRand = random.randint(0, 1)
     colorOffsetVal = True
     if booleanRand == 0:
@@ -70,8 +86,9 @@ def glitch(img):
     return glitchImage
 
 def generate_filename():
-    # Generar un nombre único para la imagen
+    # Generamos un nombre único para la imagen
     return f"glitch_{random.randint(1000, 9999)}.jpg"
 
 if __name__ == '__main__':
+    # Ejecutamos la aplicación Flask en el host 0.0.0.0 y el puerto 5000
     app.run(host='0.0.0.0', port=5000)
