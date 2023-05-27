976f4f90f0c788ddee7bf74e8749b339ed79d7d3
Tu Nombre <you@example.com>
2023-05-27 13:38:47 -0600

    Actualizacion de spinner y diseños

--- static/js/script.js
+++ static/js/script.js
@@ -5,14 +5,59 @@ function showFileName() {
   
     if (fileInput.files.length > 0) {
       const fileName = fileInput.files[0].name;
-      fileNameLabel.innerHTML = `<img id="loadImg" src="static/loadImg.png" alt="loadImg"><br>Imagen: ${fileName}`;
+      fileNameLabel.innerHTML = `Imagen: ${fileName}<br><br><img id="loadedIMG" src="/static/uploads/${fileName}" 
+                                alt="loadedIMG" style=" max-height:180px; max-width: 320px; border: solid #00536e 1px;">`;
       fileNameSpan.innerText = fileName;
       fileNameSpan.classList.remove("hide");
       fileNameSpan.classList.add("animated", "show");
     } else {
-      fileNameLabel.innerHTML = `<img id="loadImg" src="static/loadImg.png" alt="loadImg"><br>`;
+      fileNameLabel.innerHTML = `<img id="loadImg" src="/static/loadImg.png" alt="loadImg"><br>`;
       fileNameSpan.classList.remove("show");
       fileNameSpan.classList.add("animated", "hide");
     }
   }
   
+
+
+  
+
+
+  function showSpinner() {
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('hidden');
+}
+
+function hideSpinner() {
+    const spinner = document.getElementById('spinner');
+    spinner.classList.add('hidden');
+}
+
+// JavaScript para controlar la visibilidad del spinner y la imagen
+window.addEventListener('DOMContentLoaded', (event) => {
+    const spinner = document.getElementById('spinner');
+    const form = document.querySelector('.upload-form');
+    const previewImage = document.getElementById('preview-image');
+
+    // Ocultar el spinner al cargar la página
+    hideSpinner();
+
+    // Mostrar el spinner al enviar el formulario
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Evitar el envío del formulario
+        showSpinner();
+
+        // Simular un retraso para mostrar el spinner
+        setTimeout(() => {
+            form.submit(); // Enviar el formulario después de mostrar el spinner
+        }, 100);
+    });
+
+    // Ocultar el spinner cuando la imagen se cargue
+    previewImage.addEventListener('load', () => {
+        hideSpinner();
+    });
+});
+
+
+
+
